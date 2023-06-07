0b866bb6e9474c5ab4f5fac0061ac98de5d8fed2
2vedant8 <vanshgupta028@gmail.com>
2023-06-07 09:43:10 +0530

    discord website done ready to be deployed on netlify

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily: {
+    Whitney: ["Whitney"],
+    Ginto: ["Ginto"],
+    ggSans: ["ggSans"],
+  },
+}
+
